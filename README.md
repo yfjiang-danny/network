64e383c972cb6757622f053c209747c29cdec849
yfjiang.danny <jiangyf@mvalley.io>
2024-01-28 23:26:55 +0800

    feat: add grab program

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Network Learning
+
 Network Reference
+
+**QA**
+
+1. `pcap.h: No such file or directory` -> Missing lib `libpcap`,then install **libpcap**
+
+   `Ubuntu / Debian:`
+
+   ```bash
+   sudo apt-get update
+   sudo apt-get install libpcap-dev
+   ```
+
+   `Red Hat / CentOS:`
+
+   ```bash
+   sudo yum install libpcap-devel
+   ```
+
+   `macOS:`
+
+   ```bash
+   brew install libpcap
+   ```
+
+   `Windows:`
+   use `WinPcap` or `Npcap`
+
+2. `You don't have permission to capture on that device (socket: Operation not permitted)`
+
+   Use sudo: `sudo /usr/local/go/bin/go run your_program.go `, make sure `/usr/local/go/bin/go` is your go path, you can use `which go` to get it.
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"github.com/google/gopacket/pcapgo"
+)
+
+func main() {
+	// Open the network interface to capture packets
+	handle, err := pcap.OpenLive("wlp3s0", 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	// Open a new PCAP file for writing decrypted packets
+	outfile, err := os.Create("decrypted_packets.pcap")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+
+	writer := pcapgo.NewWriter(outfile)
+	writer.WriteFileHeader(1600, handle.LinkType())
+
+	for packet := range packetSource.Packets() {
+		printPacketInfo((packet))
+	}
+}
+
+func printPacketInfo(packet gopacket.Packet) {
+	// Let's see if the packet is an ethernet packet
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+		fmt.Println("Ethernet layer detected.")
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+		// Ethernet type is typically IPv4 but could be ARP or other
+		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+		fmt.Println()
+	}
+	// Let's see if the packet is IP (even though the ether type told us)
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		fmt.Println("IPv4 layer detected.")
+		ip, _ := ipLayer.(*layers.IPv4)
+		// IP layer variables:
+		// Version (Either 4 or 6)
+		// IHL (IP Header Length in 32-bit words)
+		// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
+		// Checksum, SrcIP, DstIP
+		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("Protocol: ", ip.Protocol)
+		fmt.Println()
+	}
+	// Let's see if the packet is TCP
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		fmt.Println("TCP layer detected.")
+		tcp, _ := tcpLayer.(*layers.TCP)
+		// TCP layer variables:
+		// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
+		// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
+		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+		fmt.Println("Sequence number: ", tcp.Seq)
+		fmt.Println()
+	}
+	// Iterate over all layers, printing out each layer type
+	fmt.Println("All packet layers:")
+	for _, layer := range packet.Layers() {
+		fmt.Println("- ", layer.LayerType())
+	}
+	///.......................................................
+	// Check for errors
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Println("Error decoding some part of the packet:", err)
+	}
+
+	fmt.Println("------------------------")
+}
